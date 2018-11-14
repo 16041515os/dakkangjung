6a6d68bb94660095a78859347e087d6dee26045f
kang <cse20131515@cspro9.sogang.ac.kr>
2018-11-14 00:11:04 -0800

    add not present

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -149,7 +149,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  if(user == 0 || !is_user_vaddr(fault_addr))
+  if(user == 0 || !is_user_vaddr(fault_addr) || not_present)
     thread_exit(-1);;
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
