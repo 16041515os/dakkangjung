8af290377d2644f7f8fb53a64b2f7b4806338e5a
JinwookMBP <vustthat@gmail.com>
2018-12-26 10:53:02 +0900

    resolve line endings

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-void swap_init();
+void swap_init(void);
 uint32_t swap_out(void *kpage);
 void swap_in(uint32_t swap_idx, void *kpage);
 void swap_slot_free(uint32_t swap_idx);
